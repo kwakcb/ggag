08bc887fbaf2968df5b1900b788a20949cea519c
kwakcb <58457406+kwakcb@users.noreply.github.com>
2024-07-28 20:50:44 +0900

    Add files via upload

--- ggag.py
+++ ggag.py
@@ -42,33 +42,6 @@ options = ["[NOC_10G(용량확대)]","[NOC_BAT(24)]","[NOC_CRC발생]","[NOC_PLK
 # 리스트박스 생성
 selected_option = st.selectbox("■ BS HEAD", options)
 
-# 클립보드 복사 기능을 위한 HTML과 JavaScript 코드
-copy_script = f"""
-<script>
-function copyToClipboard(text) {{
-    navigator.clipboard.writeText(text).then(function() {{
-        alert('Copied to clipboard: ' + text);
-    }}, function(err) {{
-        alert('Failed to copy text: ' + err);
-    }});
-}}
-
-// 클릭 이벤트가 발생할 때 클립보드에 텍스트를 복사
-document.addEventListener('DOMContentLoaded', function() {{
-    document.getElementById('copy-button').addEventListener('click', function() {{
-        copyToClipboard("{selected_option}");
-    }});
-}});
-</script>
-<button id="copy-button">Copy to Clipboard</button>
-"""
-
-# 클립보드 복사 버튼을 HTML로 삽입
-components.html(copy_script, height=100)
-
-# 선택된 항목을 화면에 표시
-# st.write(f"{selected_option}")
-
 #------------------------------
 # 첫 번째 리스트박스에 표시할 항목 목록 정의
 options1 = ["[KT차단기복구]", "[고객원인]","[고객측작업]","[광커넥터복구]","[기타]","[멀티탭 ON/교체]","[모듈교체]",
@@ -84,12 +57,7 @@ moss_thankyou = "수고하셨습니다~"
 options2 = [" ","[DB현행화]","[FOLLOW추가]","[문자발송]","[원격조치]", "[원인분석]","[전기작업확인(전화)]","[정전알림이]"]
 selected_option2 = st.selectbox("<NOC_선조치>", options2)
 
-# Streamlit 상태 업데이트
-st.session_state['selected_option1'] = selected_option1
-st.session_state['moss_recover'] = moss_recover
-st.session_state['selected_option2'] = selected_option2
-
-# 조건에 따라 combinedText 구성
+# 조건에 따라 combined_text 구성
 combined_text = f"{selected_option1}\n{moss_recover}\n{moss_thankyou}"
 if selected_option2 != " ":
     combined_text += f"\n<NOC_선조치> {selected_option2}"
@@ -108,7 +76,7 @@ function copyToClipboard(text) {{
 // 클릭 이벤트가 발생할 때 클립보드에 텍스트를 복사
 document.addEventListener('DOMContentLoaded', function() {{
     document.getElementById('copy-button').addEventListener('click', function() {{
-        const combinedText = `{combined_text.replace('\\n', '\\\\n')}`;
+        const combinedText = `{combined_text.replace('\n', '\\n')}`;
         copyToClipboard(combinedText);
     }});
 }});
@@ -119,6 +87,8 @@ document.addEventListener('DOMContentLoaded', function() {{
 # 클립보드 복사 버튼을 HTML로 삽입
 components.html(copy_script, height=100)
 
+# 선택된 항목을 화면에 표시
+# st.write(f"{selected_option}")
 # 선택된 항목 및 텍스트를 화면에 표시
 #st.write(f"■ 회복 HEAD: {selected_option1}")
 #st.write(f"회복내용: {moss_recover}")
