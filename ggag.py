fa04c677b253af0511cf260ce7081f370693c31e
kwakcb <58457406+kwakcb@users.noreply.github.com>
2024-08-12 16:55:18 +0900

    Add files via upload

--- ggag.py
+++ ggag.py
@@ -55,7 +55,8 @@ elif menu == "고장상황":
     // 클릭 이벤트가 발생할 때 클립보드에 텍스트를 복사
     document.addEventListener('DOMContentLoaded', function() {{
         document.getElementById('copy-button-bs-head').addEventListener('click', function() {{
-            copyToClipboard(`{combined_text_bs.replace('\\n', '\\\\n').replace('`', '\\`')}`);
+            const textToCopy = `{combined_text_bs.replace("\\n", "\\\\n").replace("`", "\\`")}`;
+            copyToClipboard(textToCopy);
         }});
     }});
     </script>
@@ -88,7 +89,7 @@ elif menu == "고장상황":
     copy_script_recover_head = f"""
     <script>
     function copyToClipboard() {{
-        const combinedText = `{combined_text_recover.replace('\\n', '\\\\n').replace('`', '\\`')}`;
+        const combinedText = `{combined_text_recover.replace("\\n", "\\\\n").replace("`", "\\`")}`;
         navigator.clipboard.writeText(combinedText).then(function() {{
             alert('클립보드에 복사되었습니다: ' + combinedText);
         }}, function(err) {{
